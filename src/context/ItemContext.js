e7eb1abd577cc4c7abdd378a7c545cabaf02d7c7
muraoka <ym.animator@gmail.com>
2022-10-06 11:30:09 +0900

    不要になった保管場所情報の削除

--- src/context/ItemContext.js
+++ src/context/ItemContext.js
@@ -3,12 +3,6 @@ import { createContext, useState, useEffect } from 'react';
 export const ItemContext = createContext();
 
 export const ItemProvider = ({ children }) => {
-  const placeList = [
-    {id: 1, name: '総務部'},
-    {id: 2, name: '開発部'},
-    {id: 3, name: '会議室'}
-  ];
-
   const API_BASE_URL = 'http://localhost:5000';
 
   const [places, setPlaces] = useState([]);
